c136baccc8e20e7ae338306a486a1bd8b8b285d8
Hayden Bird <haydenpeterbird@gmail.com>
2018-11-12 12:13:27 +1000

    Changed: Image selector now finds the relative filepath

--- core/src/com/mygdx/listeners/SelectImageAdapter.java
+++ core/src/com/mygdx/listeners/SelectImageAdapter.java
@@ -6,6 +6,10 @@ import com.kotcrab.vis.ui.widget.file.FileChooserAdapter;
 import com.mygdx.game.TableTopToken;
 import com.mygdx.managers.EngineManager;
 import com.mygdx.managers.MapManager;
+import sun.security.ssl.Debug;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SelectImageAdapter extends FileChooserAdapter {
     private final int layer;
@@ -16,8 +20,15 @@ public class SelectImageAdapter extends FileChooserAdapter {
 
     @Override
     public void selected(Array<FileHandle> file) {
+
+        String pattern = ".*(assets\\/.*)";
+        Pattern p = Pattern.compile(pattern);
+        Matcher m = p.matcher(file.get(0).path());
+        m.find();
+        String path = m.group(1);
         TableTopToken newToken = new TableTopToken(MapManager.getMapStage().getCamera().position.x, MapManager.getMapStage().getCamera().position.y,
-                file.get(0).path(), MapManager.getCurrentMap(), layer, EngineManager.getCurrentPlayer());
+                path, MapManager.getCurrentMap(), layer, EngineManager.getCurrentPlayer());
         MapManager.getCurrentMap().addToken(newToken, layer);
+        Debug.println("Path was found to be", path);
     }
 }
